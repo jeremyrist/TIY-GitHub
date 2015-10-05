62b822ddade695b7c04304e0b187d0a3a25a9cd2
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-05 17:29:43 -0400

    attempt to make the repositories tab active

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-(function(){
-  console.log($.ajax("../apis/github/users/octocat.json"));
-})();
+// (function(){
+//   console.log($.ajax("../apis/github/users/octocat.json"));
+// })();
 
 // IIFE stands for
 // Immediately-invoked function expression
@@ -8,3 +8,7 @@
 // The purple function at the top is how you start an IIFE
 
 // The function is then defined btw the curly braces
+
+$('ul.tabs').each(function(){
+  var $active = $(this).find('span');
+})
