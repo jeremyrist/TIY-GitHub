fe982aa579aef4bea366cfb091e0bd5776f1540c
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-06 16:57:45 -0400

    create the starting point with the jquery json

--- src/js/main.js
+++ src/js/main.js
@@ -47,3 +47,22 @@ $('ul.tabs').each(function(){
      e.preventDefault();
    });
  });
+
+ $.ajax({
+  dataType: "json",
+  url: "apis/github/users/jeremyrist",
+  data: data,
+  success: success
+});
+
+$.getJSON( "ajax/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
