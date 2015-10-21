2162afba9dd77f6784de9efce3844f18961620ff
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-21 19:37:16 -0400

    Make a comment section  * find the copy of my own github api  * make comments in tonight's issue to reference it on the practice page  * get the data and put in my api cache called `comments.json`

--- src/js/main.js
+++ src/js/main.js
@@ -14,104 +14,114 @@
 // })
 
 // // PREVIOUS WORKING CODE
-// $('ul.tabs').each(function(){
-//    // For each set of tabs, we want to keep track of
-//    // which tab is active and it's associated content
-//    var $active, $content, $links = $(this).find('a');
-//
-//    // If the location.hash matches one of the links, use that as the active tab.
-//    // If no match is found, use the first link as the initial active tab.
-//    $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-//    $active.addClass('active');
-//
-//    $content = $($active[0].hash);
-//
-//    // Hide the remaining content
-//    $links.not($active).each(function () {
-//      $(this.hash).hide();
-//    });
-//
-//    // Bind the click event handler
-//    $(this).on('click', 'a', function(e){
-//      // Make the old tab inactive.
-//      $active.removeClass('active');
-//      $content.hide();
-//
-//      // Update the variables with the new link and content
-//      $active = $(this);
-//      $content = $(this.hash);
-//
-//      // Make the tab active.
-//      $active.addClass('active');
-//      $content.show();
-//
-//      // Prevent the anchor's default click action
-//      e.preventDefault();
-//    });
-//  });
+$('ul.tabs').each(function(){
+   // For each set of tabs, we want to keep track of
+   // which tab is active and it's associated content
+   var $active, $content, $links = $(this).find('a');
+
+   // If the location.hash matches one of the links, use that as the active tab.
+   // If no match is found, use the first link as the initial active tab.
+   $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+   $active.addClass('active');
+
+   $content = $($active[0].hash);
+
+   // Hide the remaining content
+   $links.not($active).each(function () {
+     $(this.hash).hide();
+   });
+
+   // Bind the click event handler
+   $(this).on('click', 'a', function(e){
+     // Make the old tab inactive.
+     $active.removeClass('active');
+     $content.hide();
+
+     // Update the variables with the new link and content
+     $active = $(this);
+     $content = $(this.hash);
+
+     // Make the tab active.
+     $active.addClass('active');
+     $content.show();
+
+     // Prevent the anchor's default click action
+     e.preventDefault();
+   });
+ });
 
 
 // Time to try to get that jQuery.getJSON to work!
  // var userApiUrl = ("../../apis/github/users/jeremyrist.json");
 
-// //PREVIOUS WORKING CODE
-// $.ajax({
-//   // url: userApiUrl,
-//   url: "https://api.github.com/users/jeremyrist",
-//   dataType: 'json',
-//   success: function(json)
-//   {
-//       _.templateSettings.variable = 'm'
-//       var template = _.template($('#lodashTemp').html())
-//       var user = json;
-//       $('.test').after(template(user));
-//
-//
-//       // $('.small-column h1').text( json.name );
-//       // $('.small-column h2').text( json.login);
-//   }
-// })
+//PREVIOUS WORKING CODE
+$.ajax({
+  // url: userApiUrl,
+  url: "https://api.github.com/users/jeremyrist",
+  dataType: 'json',
+  success: function(json)
+  {
+      _.templateSettings.variable = 'm'
+      var template = _.template($('#lodashTemp').html())
+      var user = json;
+      $('.test').after(template(user));
 
+
+      // $('.small-column h1').text( json.name );
+      // $('.small-column h2').text( json.login);
+  }
+})
+
+// Homework code from John
 ;(function(){
 
  angular.module('TIY-GitHub', [ ])
    .run(function($http, $rootScope){
 
-     $http.get('apis/github/users/jmcreasman.json')
+     $http.get('/apis/github/users/jeremyrist.json')
        .then(function (response){
-         $rootScope.user = response.data;
-       });
-
+        //  $rootScope.login = response.data.login;
+         $rootScope.jeremy = response.data;
+       })
    })
 
    .run(function($http, $rootScope){
-
-     $http.get('apis/github/users/octocat/repositories.json')
+     $http.get('/apis/github/comments/comments.json')
        .then(function (response){
-         $rootScope.repo4 = response.data[4];
-       });
+        //  $rootScope.login = response.data.login;
+         $rootScope.body = response.data;
+       })
+     })
+   })(); //END IIFE
 
-   })
+  //  .run(function($http, $rootScope){
+   //
+  //    $http.get('apis/github/users/jeremyrist/repositories.json')
+  //      .then(function (response){
+  //        $rootScope.repo4 = response.data[4];
+  //      });
+   //
+  //  })
+   //
+  //  .run(function($http, $rootScope){
+   //
+  //    $http.get('apis/github/users/jeremyrist/repositories.json')
+  //      .then(function (response){
+  //        $rootScope.repo3 = response.data[3];
+  //      });
+   //
+  //  })
+   //
+  //  .run(function($http, $rootScope){
+   //
+  //    $http.get('apis/github/users/jeremyrist/repositories.json')
+  //      .then(function (response){
+  //        $rootScope.repo2 = response.data[2];
+  //      });
 
-   .run(function($http, $rootScope){
+  //  })
 
-     $http.get('apis/github/users/octocat/repositories.json')
-       .then(function (response){
-         $rootScope.repo3 = response.data[3];
-       });
-
-   })
-
-   .run(function($http, $rootScope){
-
-     $http.get('apis/github/users/octocat/repositories.json')
-       .then(function (response){
-         $rootScope.repo2 = response.data[2];
-       });
-
-   })
-
-})(); //END IIFE
+// })(); //END IIFE
 
 // lodash.template
 
